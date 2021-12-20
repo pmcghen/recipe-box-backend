cdfd0323540088f168fc316423ec16a2993ceed5
Pat McGhen <patrickmcghen@gmail.com>
2021-12-20 14:23:50 -0500

    Update settings

--- recipebox/settings.py
+++ recipebox/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("APP_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost',
                  '127.0.0.1', 'p-mcg-recipe-box.herokuapp.com']
