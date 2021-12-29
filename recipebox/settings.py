8555f8045204ea8d5cfce43f2bfaebfaea88007d
Pat McGhen <patrickmcghen@gmail.com>
2021-12-29 17:36:08 -0500

    Update allowed origins

--- recipebox/settings.py
+++ recipebox/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
-    'https://pmcghen.github.io'
+    'https://pmcghen.github.io',
+    'https://pmcg-recipe-box.herokuapp.com'
 ]
 
 MIDDLEWARE = [
