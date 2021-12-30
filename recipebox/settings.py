c9b1e859534fb593c8f49c6d431baa66fa097f7e
Pat McGhen <patrickmcghen@gmail.com>
2021-12-30 15:45:11 -0500

    Update static files settings.

--- recipebox/settings.py
+++ recipebox/settings.py
@@ -4,7 +4,7 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = env("APP_SECRET")
 
