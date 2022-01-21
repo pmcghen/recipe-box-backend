122d65c6cd4909c240a8e4ddf7745d86d0701fa8
Pat McGhen <patrickmcghen@gmail.com>
2022-01-21 10:03:02 -0500

    Change root path and add API root view. Refactor all views and serializers.

--- api/views.py
+++ api/views.py
@@ -1,15 +1,22 @@
 from django.db.models import Q
-from rest_framework import viewsets
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
 from rest_framework.views import APIView
-
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 from .serializers import RecipeSerializer, UserSerializer
 from .models import Recipe
+from .permissions import IsOwnerOrReadOnly
 
-class RecipeViewSet(viewsets.ModelViewSet):
-    queryset = Recipe.objects.all().order_by('name')
-    serializer_class = RecipeSerializer
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'recipes': reverse('recipe-list', request=request, format=format)
+    })
+
 
 @api_view(['POST'])
 def search(request):
@@ -17,12 +24,32 @@ def search(request):
 
     if query:
         recipes = Recipe.objects.filter(
-            Q(name__icontains=query) | Q(ingredients__icontains=query) | Q(directions__icontains=query) | Q(notes__icontains=query) | Q(tags__icontains=query))
+            Q(name__icontains=query) | Q(ingredients__icontains=query) | Q(directions__icontains=query) | Q(notes__icontains=query))
         serializer = RecipeSerializer(recipes, many=True)
 
         return Response(serializer.data)
     else:
         return Response({"recipes": []})
+class RecipeList(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all().order_by('name')
+    serializer_class = RecipeSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all().order_by('name')
+    serializer_class = RecipeSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class CurrentUserView(APIView):
     def get(self, request):
